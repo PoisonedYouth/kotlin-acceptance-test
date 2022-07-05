bbc195c101e890b53aac1d20029eb75ca67e36e6
Matthias Schenk <matthias.schenk@sevdesk.de>
2022-07-05 10:52:59 +0200

    Initial commit

---
+++ src/main/kotlin/com/poisonedyouth/kotlinacceptancetest/KotlinAcceptanceTestApplication.kt
@@ -0,0 +1,11 @@
+package com.poisonedyouth.kotlinacceptancetest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinAcceptanceTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinAcceptanceTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/poisonedyouth/kotlinacceptancetest/KotlinAcceptanceTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.poisonedyouth.kotlinacceptancetest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinAcceptanceTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
